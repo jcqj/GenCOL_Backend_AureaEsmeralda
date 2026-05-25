02b073daad758c50d8efa4f7517d175f00c40a6d
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-05-25 12:10:17 -0500

    Sincronizando Repo

---
+++ src/main/java/com/aureaesmeralda/AureaEsmeralda/AureaEsmeraldaApplication.java
@@ -0,0 +1,13 @@
+package com.aureaesmeralda.AureaEsmeralda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureaEsmeraldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureaEsmeraldaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aureaesmeralda/AureaEsmeralda/AureaEsmeraldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aureaesmeralda.AureaEsmeralda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureaEsmeraldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
